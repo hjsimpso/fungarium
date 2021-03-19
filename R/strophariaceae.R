5225da40fbc8484860b48aae45754102e300b18c
Hunter Simpson <hjsimpso6058@gmail.com>
2021-03-19 12:33:50 -0500

    added documentation for all dataset fields

--- R/strophariaceae.R
+++ R/strophariaceae.R
@@ -7,15 +7,23 @@
 #' @format A data frame with 89 rows and 52567 variables:
 #' \describe{
 #'   \item{id}{unique MyCoPortal ID for each record}
+#'   \item{institutionCode}{code for the collection that houses the specimen, or digital observation}
 #'   \item{scientificName}{canonical taxon name for each record}
 #'   \item{scientificNameAuthorship}{authorship name(s) associated with the canonical name}
-#'   \item{country}{country where specimen was collected or observed}
-#'   \item{stateProvince}{state or province where specimen was collected or observed}
+#'   \item{recordedBy}{name of collector(s)}
+#'   \item{associatedCollectors}{name of associated collector(s)}
+#'   \item{eventDate}{exact date of collection or observation}
 #'   \item{year}{year in which the specimen was collected or observed}
 #'   \item{occurrenceRemarks}{environmental metadata}
 #'   \item{habitat}{environmental metadata}
 #'   \item{substrate}{environmental metadata}
 #'   \item{host}{environmental metadata}
+#'   \item{country}{country where specimen was collected or observed}
+#'   \item{stateProvince}{state or province where specimen was collected or observed}
+#'   \item{county}{county in which the specimen was observed or collected}
+#'   \item{decimalLatitude}{latitude where the specimen was observed or collected}
+#'   \item{decimalLongitude}{longitude where the specimen was observed or collected}
+#'   \item{references}{link to MyCoPortal online record}
 #' }
 #' @source \url{https://mycoportal.org}
 "strophariaceae"
