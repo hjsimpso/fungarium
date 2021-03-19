9dac9d49df6f953560da85b26e68094eecb9ef78
Hunter Simpson <hjsimpso6058@gmail.com>
2021-03-19 12:55:16 -0500

    fixed smaller dataset

--- data-raw/strophariaceae.R
+++ data-raw/strophariaceae.R
@@ -1,7 +1,7 @@
 ## code to prepare `strophariaceae` dataset goes here
 strophariaceae <- data.table::fread("C:/Users/hjs92/Documents/git_repos/fungarium/data-raw/strophariaceae.tab",
                                     colClasses = "character", encoding = "Latin-1", quote = "")
-data <- strophariaceae[sample(1:nrow(strophariaceae), 10000), c("id","institutionCode",
+strophariaceae <- strophariaceae[sample(1:nrow(strophariaceae), 10000), c("id","institutionCode",
                                                                 "scientificName",
                                                                 "scientificNameAuthorship",
                                                                 "recordedBy",
