4a1a749220e32e5bc1ed592ee42ca32648a5dd4f
Hunter Simpson <hjsimpso6058@gmail.com>
2021-04-02 15:21:26 -0500

    added string_clean; condensed example

--- R/find_trait.R
+++ R/find_trait.R
@@ -14,13 +14,14 @@
 #' @param pos_string     Character string ("positive") containing a \link{regular expression} that is used to find character strings within the specified metadata columns of the input data frame that contain trait-relevant keywords or phrases.
 #' @param neg_string     Character string ("negative") containing a regular expression that is used to remove records that were falsely identfied, via the "positive" search string, as being trait-relevant. This argument is optional.
 #' @param metadata_cols  Character vector containing names of columns with trait-relevant metadata. Default names are "occurrenceRemarks", "host", "habitat", and "substrate" (names used in MyCoPortal datasets).
+#' @param string_clean   Logical. If TRUE (the default), strings in metadata_cols are "cleaned" prior to trait searching. This includes converting strings to lowercase and removing any puncuation (e.g., periods, commas, question marks, etc.) or extra white space.
 #' @return           Returns a dataframe of records with trait-relevant metadata.
 #' @export
 #'
 #' @examples
 #' library(fungarium)
 #' data(strophariaceae) #import sample dataset
-#' MP_data <- taxon_update(strophariaceae) #update taxon names
+#' data <- strophariaceae
 #'
 #' #Finds fire-associated records
 #' string1 <- "(?i)charred|burn(t|ed)|scorched|fire.?(killed|damaged|scarred)|killed.by.fire"
@@ -29,16 +30,23 @@
 #' string2 <- "(?i)un.?burn(t|ed)"
 #'
 #' #filter out records that do not contain any environmental metadata (optional)
-#' MP_data <- MP_data[MP_data$occurrenceRemarks!=""|MP_data$host!=""|
-#'                    MP_data$habitat!=""|MP_data$substrate!="",]
+#' data <- data[data$occurrenceRemarks!=""|data$host!=""|
+#'                    data$habitat!=""|data$substrate!="",]
 #'
 #' #find trait-relevant records
-#' trait_data <- find_trait(MP_data, pos_string=string1, neg_string=string2)
+#' trait_data <- find_trait(data, pos_string=string1, neg_string=string2)
 
-find_trait <- function(data, pos_string, metadata_cols=c("host", "substrate", "habitat", "occurrenceRemarks"), neg_string=NULL){
+find_trait <- function(data, pos_string, metadata_cols=c("host", "substrate", "habitat", "occurrenceRemarks"), neg_string=NULL, string_clean=TRUE){
   #Check that the input is formatted correctly. If not, print error.
   if (!is.data.frame(data)){
     stop('Input data needs to be a data.frame.')
+  }
+
+  #clean strings
+  if (string_clean){
+    for (i in 1:length(metadata_cols)){
+      data[[metadata_cols[i]]] <- str_clean(data[[metadata_cols[i]]])
+    }
   }
 
   #Search for records with positive keywords/phrases
