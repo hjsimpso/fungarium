d7fdf13d85c02b9a64ad8f0b09be053378da3e40
Hunter Simpson <hjsimpso6058@gmail.com>
2021-04-29 09:48:45 -0500

    added metadata_cols exists check

--- R/find_trait.R
+++ R/find_trait.R
@@ -11,11 +11,15 @@
 #'
 #'
 #' @param data           Data.frame containing columns of trait-relevant metadata.
+#' @param metadata_cols  Character vector containing names of columns with trait-relevant metadata.
 #' @param pos_string     Character string ("positive") containing a \link{regular expression} that is used to find character strings within the specified metadata columns of the input data frame that contain trait-relevant keywords or phrases.
-#' @param neg_string     Character string ("negative") containing a regular expression that is used to remove records that were falsely identfied, via the "positive" search string, as being trait-relevant. This argument is optional.
-#' @param metadata_cols  Character vector containing names of columns with trait-relevant metadata. Default names are "occurrenceRemarks", "host", "habitat", and "substrate" (names used in MyCoPortal datasets).
-#' @param string_clean   Logical. If TRUE (the default), strings in metadata_cols are "cleaned" prior to trait searching. This includes converting strings to lowercase and removing any puncuation (e.g., periods, commas, question marks, etc.) or extra white space.
+#' @param neg_string     Character string ("negative") containing a regular expression that is used to remove records that were falsely identified, via the "positive" search string, as being trait-relevant. This argument is optional.
+#' @param string_clean   Logical. If TRUE (the default), strings in metadata_cols are "cleaned" prior to trait searching. This includes converting strings to lowercase and removing any punctuation (e.g., periods, commas, question marks, etc.) or extra white space.
 #' @return           Returns a data.frame of records with trait-relevant metadata.
+#' @note Current trait-relevant fields (i.e., \code{metadata_cols}) in MyCoPortal data include "occurrenceRemarks",
+#' "habitat", and "substrate". The "host" field, which contained trait-relevant info, was present in older MyCoPortal datasets, but has been removed as of April 2021.
+#' It is not yet clear where this data was moved to. It's possible that it was moved to the "associatedTaxa" field. This field already existed in MyCoPortal datasets but was typically blank.
+#' Either way, it appears that "associatedTaxa" may be a useful field to use for \code{metadata_cols}; however, this may depend on the trait of interest, so use your best judgment.
 #' @export
 #'
 #' @examples
@@ -26,20 +30,28 @@
 #' #Finds fire-associated records
 #' string1 <- "(?i)charred|burn(t|ed)|scorched|fire.?(killed|damaged|scarred)|killed.by.fire"
 #'
-#' #Removes records falsely identfied as fire-associated
+#' #Removes records falsely identified as fire-associated
 #' string2 <- "(?i)un.?burn(t|ed)"
 #'
 #' #filter out records that do not contain any environmental metadata (optional)
+#' #note that "host" is no longer present in new MyCoPortal data sets
 #' data <- data[data$occurrenceRemarks!=""|data$host!=""|
 #'                    data$habitat!=""|data$substrate!="",]
 #'
 #' #find trait-relevant records
-#' trait_data <- find_trait(data, pos_string=string1, neg_string=string2)
+#' trait_data <- find_trait(data, c("occurrenceRemarks", "host", "habitat", "substrate"),
+#'                          pos_string=string1, neg_string=string2)
 
-find_trait <- function(data, pos_string, metadata_cols=c("host", "substrate", "habitat", "occurrenceRemarks"), neg_string=NULL, string_clean=TRUE){
+find_trait <- function(data, metadata_cols, pos_string,  neg_string=NULL, string_clean=TRUE){
   #Check that the input is formatted correctly. If not, print error.
   if (!is.data.frame(data)){
     stop('Input data needs to be a data.frame.')
+  }
+  #Check that all metadata cols exist in input data
+  for (i in 1:length(metadata_cols)){
+    if (!metadata_cols[i]%in%colnames(data)){
+      stop(paste0("'",metadata_cols[i],"'", " field not found in input data. Please use valid values for metadata_cols."))
+    }
   }
 
   #clean strings
