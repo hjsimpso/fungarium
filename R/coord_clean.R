44b4575d876b21ecdd81de807ece775a6f3ea749
Hunter Simpson <hjsimpso6058@gmail.com>
2021-12-13 15:19:34 -0500

    new date/coordinate cleaning functions

--- R/coord_clean.R
+++ R/coord_clean.R
@@ -1,34 +1,49 @@
-#' Clean lat/long coordinates in fungal collections data sets
+#' Clean lat/long coordinates in occurrence data sets
 #'
-#' Parses variably formatted dates into a consistent format of specified resolution (e.g., year, month, day) and removes problematic dates that cannot be parsed.
+#' Removes records that do not pass various coordinate validity tests.
 #'
 #'
-#' @param data Data.frame of fungal collections data set (e.g. MyCoPortal, GBIF)
+#' @param data Data.frame of occurrence data (e.g. MyCoPortal, GBIF) that includes decimal latitude and longitude
 #' @param lat Character string specifying the decimal latitude column. Default is "decimalLatitude" (i.e., the Darwin Core standard).
 #' @param lon Character string specifying the decimal longitude column. Default is "decimalLongitude" (i.e., the Darwin Core standard).
-#' @param tests Character vector specifying the coordinates cleaning tests to perform (i.e., "zero", "equal", "countries", "centroids"). Default is c("zero", "equal", "countries", "centroids").
+#' @param tests Character vector specifying the coordinate cleaning tests to perform. Options include: "zero", "equal", "countries", "centroids". Default is c("zero", "equal", "countries", "centroids").
+#' @param country Character string specifying the name of the country column. Default is "country" (i.e., the Darwin Core standard).
+#' @param centroid_dis Numeric specifying the distance threshold (in meters) to use for the centroid test. Default is 100.
+#' @param round_digits Integer specifying the number of decimal places to use for rounding coordinates. Default is 4. If NULL, no rounding is performed.
 #'
-#' @return Data.frame containing the input data set with the following output fields appended.
-#' Records with dates that could not be parsed are removed.
-#' \item{date_fixed}{NULL}
-#' \item{parsed_date}{NULL}
-#' \item{parsed_format}{NULL}
-#' \item{year_fixed}{NULL}
-#' \item{month_fixed}{NULL}
-#' \item{day_fixed}{NULL}
-#' @note Full dates other than the "Ymd" format are all assumed to be in the "dmY" format, not "mdY". Ex: 01/10/1990 is assumed to be October 10, 1990, not January 10, 1990.
+#' @details The following tests are automatically done:
+#' \describe{
+#'  \item{\code{non-numeric}}{lat or lon are not numeric or cannot be converted to numeric}
+#'  \item{\code{non-valid}}{lat or lon are invalid numbers (i.e., lat>90, lat<-90, lon>180, lon<-180)}
+#' }
+#' The following tests can be selected:
+#' \describe{
+#'  \item{\code{zero}}{lat and lon are both zero}
+#'  \item{\code{equal}}{lat and lon are equal}
+#'  \item{\code{countries}}{point is outside the bounds of the country listed}
+#'  \item{\code{centroid}}{distance between point and country centroid is less than or equal to the centroid_dis specified}
+#' }
+#' @return Data.frame containing records from the input data set that passed the coordinate cleaning tests. Number of records removed at each step is printed to the console.
 #' @export
 #'
 #' @examples
 #' library(fungarium)
-#' data(strophariaceae) #import sample data set
-#' data <- date_clean(data) #clean dates
+#' data(agaricales) #import sample data set
+#' clean <- coord_clean(agaricales) #clean dates
 #'
 
 coord_clean <- function(data, lat="decimalLatitude", lon="decimalLongitude", country="country",
-                        tests=c("zero", "equal", "countries", "centroids", "capitals"),
+                        tests=c("all"),
                         centroid_dis=100,
                         round_digits=4){
+  #check that the input is formatted correctly. If not, stop and print error.
+  if (!is.data.frame(data)){
+    stop('Input data needs to be a data.frame.')
+  }
+
+  if(tests=="all"){
+    tests <- c("zero", "equal", "countries", "centroids")
+  }
   if(!is.null(centroid_dis)){
     centroid_dis <- units::as_units(centroid_dis, "m")
   }
@@ -78,13 +93,16 @@ coord_clean <- function(data, lat="decimalLatitude", lon="decimalLongitude", cou
     data$row_numb <- within
     rm(within)
     shp2 <- as.data.frame(shp)
-    shp2 <- subset(shp2,select=-c(geometry))
+    #shp2 <- subset(shp2,select=-c(geometry))
+    shp2 <- shp2[,!colnames(shp2)%in%"geometry"]
     shp2$row_numb <- 0:(nrow(shp2)-1) #shapefile row labels start at 0
-    data2 <- subset(as.data.frame(data), select=c(row_numb,country))
+    data2 <- as.data.frame(data)
+    data2 <- data2[,c("row_numb",country)]
     data2 <- dplyr::left_join(data2, shp2, by="row_numb")
     check <- lapply(as.list(as.data.frame(t(data2))),
                     country_check)
-    rm(shp2)
+
+    rm(shp2,data2)
     check <- as.logical(check)
     data <- data[check,]
     message(paste0("'countries' test: ", (row1-nrow(data)), " records removed."))
@@ -106,7 +124,7 @@ coord_clean <- function(data, lat="decimalLatitude", lon="decimalLongitude", cou
 
 
 country_check <- function(x){
-  x <- fungarium:::str_clean(x)
+  x <- str_clean(x)
   out <- x[2]%in%x[3:length(x)]
   return(out)
 }
--- R/date_clean.R
+++ R/date_clean.R
@@ -1,5 +1,32 @@
+#' Clean dates in occurrence data sets
+#'
+#' Parses dates with varying format into a consistent format of specified resolution (e.g., year, month, day) and removes problematic dates that cannot be parsed.
+#'
+#'
+#' @param data Data.frame of occurrence data set (e.g. MyCoPortal, GBIF)
+#' @param date_col Character string specifying the date column. Default is "eventDate" (i.e., the Darwin Core standard).
+#' @param date_res Character string specifying the desired resolution for parsed dates (i.e., "year", "month", "day"). Default is "year".
+#'
+#' @return Data.frame containing the input data set with the following output fields appended.
+#' Records with dates that could not be parsed are removed.
+#' \describe{
+#' \item{\code{date_fixed}}{Variabe date formats are transformed into standard YYYY-MM-DD format. Dates limited to year or month resolution (e.g., 1990-00-00 or 1990-01-00), are transformed to YYYY or YYYY-MM format respectively.}
+#' \item{\code{parsed_date}}{"date_fixed" values are parsed into standard POSIXct date-time objects using \code{lubridate::parse_date_time}}
+#' \item{\code{parsed_format}}{Detected date format (e.g., "Y", "Ym", or "Ymd"). Used for parsing dates correctly.}
+#' \item{\code{year_fixed}}{The collection year value after cleaning and parsing dates.}
+#' \item{\code{month_fixed}}{The collection month value after cleaning and parsing dates.}
+#' \item{\code{day_fixed}}{The collection day value after cleaning and parsing dates.}
+#' }
+#' @note Full dates other than the "Ymd" format are all assumed to be in the "dmY" format, not "mdY". Ex: 01/10/1990 is assumed to be October 10, 1990, not January 10, 1990.
+#' @export
+#'
+#' @examples
+#' library(fungarium)
+#' data(agaricales) #import sample data set
+#' clean <- date_clean(agaricales) #clean dates
+#'
 
-##date clean
+
 date_clean <- function(data, date_col="eventDate", date_res="year"){
   if (date_res=="year"){
     date_res <- "Y"
@@ -13,7 +40,7 @@ date_clean <- function(data, date_col="eventDate", date_res="year"){
                        abbr=c("Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"),
                        number=c("01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"))
   data$date_fixed <- gsub("^(-|(0|00|0000)(/|\\.|-)(0|00)(/|\\.|-)(0|00|0000))$", "", data[[date_col]])#remove null dates
-  data$date_flag <- ifelse(data$date_fixed=="", "null", "")
+  data$parsed_format <- ifelse(data$date_fixed=="", "null", "")
   data$date_fixed <- gsub("^([0-9]+)(\\.|/)([0-9]+)(\\.|/)([0-9].*)", "\\1-\\3-\\5", data$date_fixed)#fix sep character
   data$date_fixed <- gsub("^([0-9][0-9][0-9][0-9])-(00|0)-([0-9][0-9]|[0-9])(\\s|/|T|$)", "\\1\\4", data$date_fixed)#remove 00 months (Ymd)
   data$date_fixed <- gsub("^([0-9][0-9]|[0-9])-(00|0)-([0-9][0-9][0-9][0-9])", "\\3", data$date_fixed)#remove 00 month (dmY)
@@ -22,33 +49,33 @@ date_clean <- function(data, date_col="eventDate", date_res="year"){
   data$date_fixed <- gsub("^([0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])(|(T|\\s)[0-9][0-9]:[0-9][0-9]:[0-9][0-9]).*)/\\1", "\\1", data$date_fixed)#fix double dates
   data$date_fixed <- gsub("(T|\\s)00:00:00(|\\.0+)", "", data$date_fixed)#fix null time
 
-  data[grep("^[0-9][0-9][0-9][0-9]$", data$date_fixed),"date_flag"] <- "Y"
-  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])$", data$date_fixed),"date_flag"] <- "Ym"
-  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])$", data$date_fixed),"date_flag"] <- "Ymd"
-  data[grep("^[0-9][0-9][0-9][0-9]-(1[3-9]|[2-9][0-9])($|-)", data$date_fixed),"date_flag"] <- "bad_month" #for Ymd or Ym
-  data[grep("^([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])-[0-9][0-9][0-9][0-9]$", data$date_fixed),"date_flag"] <- "dmY"
-  data[grep(tolower(paste(paste0("([0-9][0-9]|[0-9])\\s", months$word), paste0("([0-9][0-9]|[0-9])\\s", months$abbr), collapse="|", sep="|")), tolower(data$date_fixed)),"date_flag"] <- "dmY" #dmonthY - for month words
-  data[grep("^([0-9][0-9]|[0-9])-(1[3-9]|[2-9][0-9])-[0-9][0-9][0-9][0-9]", data$date_fixed),"date_flag"] <- "bad_month" #for dmY
-  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])(T|\\s)[0-9][0-9]:[0-9][0-9]:[0-9][0-9]", data$date_fixed),"date_flag"] <- "YmdT"
-  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])(T|\\s)[0-9][0-9]:[0-9][0-9]:[0-9][0-9](\\+|-)", data$date_fixed),"date_flag"] <- "YmdTz"
-  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])((T|\\s)[0-9][0-9]:[0-9][0-9]:[0-9][0-9]|)/[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])((T|\\s)[0-9][0-9]:[0-9][0-9]:[0-9][0-9]|)", data$date_fixed),"date_flag"] <- "double_date" #when double dates aren't equal
+  data[grep("^[0-9][0-9][0-9][0-9]$", data$date_fixed),"parsed_format"] <- "Y"
+  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])$", data$date_fixed),"parsed_format"] <- "Ym"
+  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])$", data$date_fixed),"parsed_format"] <- "Ymd"
+  data[grep("^[0-9][0-9][0-9][0-9]-(1[3-9]|[2-9][0-9])($|-)", data$date_fixed),"parsed_format"] <- "bad_month" #for Ymd or Ym
+  data[grep("^([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])-[0-9][0-9][0-9][0-9]$", data$date_fixed),"parsed_format"] <- "dmY"
+  data[grep(tolower(paste(paste0("([0-9][0-9]|[0-9])\\s", months$word), paste0("([0-9][0-9]|[0-9])\\s", months$abbr), collapse="|", sep="|")), tolower(data$date_fixed)),"parsed_format"] <- "dmY" #dmonthY - for month words
+  data[grep("^([0-9][0-9]|[0-9])-(1[3-9]|[2-9][0-9])-[0-9][0-9][0-9][0-9]", data$date_fixed),"parsed_format"] <- "bad_month" #for dmY
+  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])(T|\\s)[0-9][0-9]:[0-9][0-9]:[0-9][0-9]", data$date_fixed),"parsed_format"] <- "YmdT"
+  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])(T|\\s)[0-9][0-9]:[0-9][0-9]:[0-9][0-9](\\+|-)", data$date_fixed),"parsed_format"] <- "YmdTz"
+  data[grep("^[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])((T|\\s)[0-9][0-9]:[0-9][0-9]:[0-9][0-9]|)/[0-9][0-9][0-9][0-9]-([0-9][0-9]|[0-9])-([0-9][0-9]|[0-9])((T|\\s)[0-9][0-9]:[0-9][0-9]:[0-9][0-9]|)", data$date_fixed),"parsed_format"] <- "double_date" #when double dates aren't equal
   rows1 <- nrow(data)
-  data <- data[data$date_flag!="null",]
+  data <- data[data$parsed_format!="null",]
   null_rows <- rows1-nrow(data)
-  data <- data[!data$date_flag%in%c("", "bad_month", "double_date"),]
+  data <- data[!data$parsed_format%in%c("", "bad_month", "double_date"),]
   error1_rows <- rows1-null_rows-nrow(data)
-  data <- data[grep(date_res, data$date_flag),]
+  data <- data[grep(date_res, data$parsed_format),]
   res_rows <- rows1-null_rows-error1_rows-nrow(data)
-  data$parsed_dates <- lubridate::parse_date_time(data$date_fixed, unique(data$date_flag))
-  data <- data[!is.na(data$parsed_dates),]
+  data$parsed_date <- lubridate::parse_date_time(data$date_fixed, unique(data$parsed_format))
+  data <- data[!is.na(data$parsed_date),]
   error2_rows <- rows1-null_rows-error1_rows-res_rows-nrow(data)
   if(date_res=="Y"){
-    data$year_fixed <- lubridate::year(data$parsed_dates)
+    data$year_fixed <- lubridate::year(data$parsed_date)
   }else if(date_res=="m"){
-    data$year_fixed <- lubridate::year(data$parsed_dates)
+    data$year_fixed <- lubridate::year(data$parsed_date)
     data$month_fixed <- lubridate::month(data$parsed_date)
   }else if(date_res=="d"){
-    data$year_fixed <- lubridate::year(data$parsed_dates)
+    data$year_fixed <- lubridate::year(data$parsed_date)
     data$month_fixed <- lubridate::month(data$parsed_date)
     data$day_fixed <- lubridate::day(data$parsed_date)
   }
