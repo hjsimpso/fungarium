1b59135e3a1b3e4bbf083ba6499cb5b345a6dbd7
hjsimpso <hjsimpso6058@gmail.com>
2025-08-14 11:54:31 -0500

    cleaning up old package functions

--- R/myswitch.R
+++
@@ -1,9 +0,0 @@
-#mycoportal_tab helper function - helps navigate to MyCoPortal download window
-
-myswitch <- function (remDr, windowId)
-{
-  qpath <- sprintf("%s/session/%s/window", remDr$serverURL,
-                   remDr$sessionInfo[["id"]])
-  remDr$queryRD(qpath, "POST", qdata = list(handle = windowId))
-}
-
