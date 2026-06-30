7bc94867d3a46ecdc7e362a1ea848937b620eea3
hjsimpso <hjsimpso6058@gmail.com>
2026-06-30 10:32:19 -0500

    created documentation for fomitopsidaceae example dataset

---
+++ R/fomitopsidaceae.R
@@ -0,0 +1,8 @@
+#' Fomitopsidaceae occurrence records for the United States
+#'
+#' Data downloaded from MyCoPortal on 6/29/2026 in Darwin Core Archive format.
+#'
+#' @format Data.frame with Darwin Core Archive fields.
+#' 
+#' @source \url{https://mycoportal.org/portal/}
+"fomitopsidaceae"
